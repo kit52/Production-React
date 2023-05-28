e487ccf280bb8bde7bad3672a55bed8f17a7effb
Kit <lesha.sinitsin@mail.ru>
2023-05-28 15:39:37 +0300

    fix test

--- src/widgets/Sidebar/ui/Sidebar/Sidebar.test.tsx
+++ src/widgets/Sidebar/ui/Sidebar/Sidebar.test.tsx
@@ -1,16 +1,16 @@
 import { fireEvent, render, screen } from '@testing-library/react';
 import { withTranslation } from 'react-i18next';
-import { renderWithTranslation }
-  from 'shared/lib/tests/renderWIthTranslation/renderWIthTranslation';
+import { componentRender } from 'shared/lib/tests/componentRender/componentRender';
+import { renderWithTranslation } from 'shared/lib/tests/renderWIthTranslation/renderWIthTranslation';
 import { Sidebar } from 'widgets/Sidebar/ui/Sidebar/Sidebar';
 
 describe('Sidebar', () => {
   test('btn text', () => {
-    renderWithTranslation(<Sidebar />);
+    componentRender(<Sidebar />);
     expect(screen.getByTestId('sidebar')).toBeInTheDocument();
   });
   test('test toggle', () => {
-    renderWithTranslation(<Sidebar />);
+    componentRender(<Sidebar />);
     const toggleBtn = screen.getByTestId('sidebar-toggle');
     expect(screen.getByTestId('sidebar-toggle')).toBeInTheDocument();
     fireEvent.click(toggleBtn);
