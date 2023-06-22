b3ffa28b8d8aed6aa3e752bf712e1922e274cd47
Kit <lesha.sinitsin@mail.ru>
2023-06-23 02:06:12 +0300

    story

---
+++ src/entities/Profile/ui/ProfileCard/ProfileCard.stories.tsx
@@ -0,0 +1,34 @@
+import React from 'react';
+import { ComponentMeta, ComponentStory } from '@storybook/react';
+import { ThemeDecorator } from 'shared/config/storybook/ThemeDecorator/ThemeDecorator';
+import { Theme } from 'app/providers/ThemeProvider';
+import { ProfileCard } from './ProfileCard';
+import { Country } from 'entities/Country';
+import { Currency } from 'entities/Currency';
+
+export default {
+  title: 'entities/ProfileCard',
+  component: ProfileCard,
+  argTypes: {
+    backgroundColor: { control: 'color' },
+  },
+  args: {
+    to: '/',
+  },
+} as ComponentMeta<typeof ProfileCard>;
+
+const Template: ComponentStory<typeof ProfileCard> = (args) => (
+  <ProfileCard {...args} />
+);
+
+export const Primary = Template.bind({});
+Primary.args = {
+  data: {
+    username: 'admin',
+    age: 22,
+    country: Country.Belarus,
+    lastName: 'last',
+    city: 'city',
+    currency: Currency.EUR,
+  },
+};
