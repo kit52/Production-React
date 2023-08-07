39bdf69760fc388563bb656837b7f80c70605336
Kit <lesha.sinitsin@mail.ru>
2023-08-07 14:52:31 +0300

    log

--- src/widgets/Sidebar/model/selectors/getSidebarItems.tsx
+++ src/widgets/Sidebar/model/selectors/getSidebarItems.tsx
@@ -21,8 +21,7 @@ export const getSidebarItems = createSelector(getUserAuthData, (user) => {
       text: 'О сайте',
     },
   ];
-  console.log('ssssssssssssssssssssssssssssss');
-  console.log(user);
+
   if (user) {
     sidebarItems.push(
       {
